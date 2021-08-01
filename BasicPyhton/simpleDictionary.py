f018d52af34944a4ccc66869d1a46990c5a801ff
ShaunAshraf <33549886+meavik@users.noreply.github.com>
2021-08-01 19:04:07 +0600

    Create simpleDictionary.py

---
+++ BasicPyhton/simpleDictionary.py
@@ -0,0 +1,9 @@
+dictionary = {
+    "organise":"to make arrangements for something to happen",
+    "orchard":"an area of land where fruit trees are grown",
+    "heroism":"great courage",
+    "duly":"in the correct way or at the correct time",
+    "sovereign":"a king or queen"
+}
+keyword = input("Enter a word to search: ")
+print(keyword," : ",dictionary[keyword])
